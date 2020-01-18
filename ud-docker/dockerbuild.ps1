b7c9d949f284d40d1ace124d8ff364166128bce8
mieel <46019913+mieel@users.noreply.github.com>
2020-01-18 15:49:41 +0100

    Create dockerbuild.ps1

---
+++ ud-docker/dockerbuild.ps1
@@ -0,0 +1,27 @@
+# BUILD IMAGE
+$tag = 'iis-ud'
+Try {
+    docker build -t $tag .
+} Catch {
+    Throw $_
+}
+
+# CREATE CONTAINER
+$container = docker run -d -p 80 $tag
+
+$containerPort = docker port $container
+
+$PortNumber = $containerPort | ForEach-Object {
+    $sc = $containerPort.IndexOf(':')+1; $containerPort.Substring($sc,$containerPort.Length-$sc)
+}
+
+Write-Host "Container $container is running on port $portNumber"
+
+# TEST
+Describe 'response' {
+    $Response = Invoke-WebRequest http://localhost:$PortNumber
+    it 'should return Expected content' {
+        $Response.Content | Should -match 'Universal Dashboard'
+    }
+}
+
